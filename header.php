7936596915c54e4d3809e3047e5a45ad52676e00
darthradek <radoslawhaller@gmail.com>
2020-11-23 18:34:57 +0100

    header first iteration

--- header.php
+++ header.php
@@ -11,12 +11,8 @@
     <body <?php body_class();?>>
         <div class="container">
             <header class="site-header">
-                <h1><a href="<?php echo home_url() ?>"><?php bloginfo('name')?></a></h1>
-                <h5><?php bloginfo('description')?></h5>
                 <nav class="site-nav">
-                    <h1 class="test">test test test test</h1>
+                    <img class="nav-logo" src="<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/OSTLOGO.png" alt="">
                     <?php wp_nav_menu(array("theme_location" => "header_menu"))?>
                 </nav>
-
-
             </header>
