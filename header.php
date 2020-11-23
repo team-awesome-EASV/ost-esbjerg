2a20569b073eea33961f200294d6fe1f189dc2fb
darthradek <radoslawhaller@gmail.com>
2020-11-23 15:54:29 +0100

    tsts

--- header.php
+++ header.php
@@ -12,9 +12,9 @@
         <div class="container">
             <header class="site-header">
                 <h1><a href="<?php echo home_url() ?>"><?php bloginfo('name')?></a></h1>
-                <h5><?php bloginfo('description')?> </h5>
+                <h5><?php bloginfo('description')?></h5>
                 <nav class="site-nav">
-
+                    <h1 class="test">test test test test</h1>
                     <?php wp_nav_menu(array("theme_location" => "header_menu"))?>
                 </nav>
 
