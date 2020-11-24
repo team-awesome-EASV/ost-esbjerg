e6f075a67ee21fb5ff50b23784b519777a176681
Johannes <ngiih91@gmail.com>
2020-11-24 10:53:02 +0100

    Update footer
    Changed the paragraph to unlisted list

--- footer.php
+++ footer.php
@@ -1,11 +1,23 @@
 <footer class="site-footer">
-    
-        <h4>Undersider</h4>
-        <h4>Åbningstider</h4>
-            <p>Man - Fre: 10:00 - 17:00</p>
-            <p>Lør: 09:00 - 13:00</p>
-            <p>Søn: Lukket</p>
-        <h4>Kontakt</h4>
+    <div>
+        <ul>Undersider</ul>
+            <li>Hjem</li>
+            <li>Produkter</li>
+            <li>Nyheder</li>
+            <li>Om os</li>
+    </div>
+    <div>
+        <ul>Åbningstider</ul>
+            <li>Man - Fre: 10:00 - 17:00</li>
+            <li>Lør: 09:00 - 13:00</li>
+            <li>Søn: Lukket</li>
+    </div>
+    <div>
+        <ul>Kontakt<ul>
+            <li>Torvegade 38 6700, Esbjerg</li>
+            <li>+45 75 12 20 57</li>
+            <li>kontakt@ostesbjerg.dk</li>
+    </div>
         <p>
         <?php bloginfo('name');?> - &copy; <?php echo date('Y'); ?></p>
 
