6e62c0953badb46fedbef6466cde99e468502d90
Raul-Octavina Albu <alburaul@gmil.com>
2020-11-24 11:35:42 +0100

    products page 50% done

--- page-products.php
+++ page-products.php
@@ -1,28 +1,213 @@
 <?php get_header()?>
 <main class="products">
-    <?php if (have_posts()): ?>
-
-    <?php while (have_posts()): the_post()?>
-    <div class="products">
-        <nav class="site-nav">
-
-            <?php $args = array(
-        "child_of" => get_top_ancestor_id(),
-        "title_li" => "",
-    );
-    ?>
-            <ul>
-                <?php wp_list_pages($args)?>
-            </ul>
-        </nav>
-
-        <h3> <?php the_title()?> </h3>
-
-        <?php the_content()?>
-
-        <?php endwhile;?>
+    <div class="hero">
+        <div class="hero-text">
+            <h1> Vores produkter</h1>
+            <div class="links-container">
+                <ul>
+                    <li>
+                        <a href="#tapas">
+                            <p class="product-link">Tapas</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#pizzaKit">
+                            <p class="product-link">Pizza kit</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#oster">
+                            <p class="product-link">Oster</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
+    <section>
+        <article id="tapas">
 
-    <?php endif;?>
+
+            <div class="product-info">
+
+                <section class="image">
+                    <h2>Tapas</h2>
+                    <div class="product-image">
+                        <img src="<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/tapas.jpg"
+                            alt="picture representing tapas plates, filled with cheese, salami, fruits, and seeds "
+                            title="tapas board">
+                    </div>
+                    <div class="tapas-price">
+                        <div>
+                            <p class="price">120</p>
+                            <p class="currency">DKK</p>
+                            <p class="extra-info">Per person</p>
+                        </div>
+                    </div>
+                </section>
+                <section class="tapas-product-text">
+                    <div class="product-text-container">
+                        <h3>Tapas anretning</h3>
+                        <p>Vores lækre tapas anretning kan bestilles fra en person og op efter, nok til en hel måltid Du
+                            får forskellige oste og charcuteri. Samt syltet grønt frugte. Vi tager også hensyn til
+                            allergi. Sig til når du bestiller. </p>
+
+                    </div>
+                    <div class="product-ingredients-list">
+                        <h3>Indenholder</h3>
+                        <ul>
+
+                            <li>
+                                <div></div>
+                                <p>4-5 forskellige oste</p>
+                            </li>
+                            <li>
+                                <div>
+
+                                </div>
+                                <p>4-5 slags charcuterie</p>
+                            </li>
+                            <li>
+                                <div></div>
+                                <p>Oliven og syltet grønt</p>
+                            </li>
+                            <li>
+                                <div></div>
+                                <p>Forskellige dips</p>
+                            </li>
+                            <li>
+                                <div></div>
+                                <p>Frugt</p>
+                            </li>
+
+                        </ul>
+
+                        <div class="tapas-line-ornament"></div>
+
+                    </div>
+
+                    <div class="contact-info">
+                        <p>Place din order her</p>
+                        <button class="contact-phone" title="call 004575122057 to place your order">
+                            <div></div>
+                            <p class="phone-number">
+
+                                <span>+</span>
+                                <span>45</span>
+                                <span>75</span>
+                                <span>12</span>
+                                <span>20</span>
+                                <span>57</span>
+                            </p>
+                        </button>
+                    </div>
+
+                </section>
+
+            </div>
+
+        </article>
+        <div class="product-separator-container-olives">
+            <div class="product-separator">
+            </div>
+        </div>
+        <article id="pizzaKit">
+            <div class="product-info">
+                <section class="image">
+                    <div class="product-image-container">
+                        <h2>Pizza Kit</h2>
+                        <div class="pizza-triangle"></div>
+                        <div class="pizza-image">
+                            <img src="" alt="image representing a beaked pizza" title="pizza kit">
+                        </div>
+                        <div class="pizza-price">
+                            <div>
+                                <p class="price">60</p>
+                                <p class="currency">DKK</p>
+                                <p class="extra-info">Per pizza kit</p>
+                            </div>
+                        </div>
+                        <div class="pizza-line-ornament"></div>
+                    </div>
+                </section>
+                <section class="pizza-product-text">
+                    <div class="product-text-container">
+                        <h3>Pizza dage</h3>
+                        <p>Vi sælger pizzakits to dage om ugen, Torsdag og Fredag.
+                            Du køber en kit fra os, som du selv kan lave. Vi har instruktionelle videoer hernede.
+                            Gerne ordr fremadrettet, sådan så vi kan forberede din kit </p>
+
+                    </div>
+                    <div class="product-ingredients-list">
+                        <h3>Indenholder</h3>
+                        <ul>
+
+                            <li>Pizzadej lavet på surdej</li>
+                            <li>Hjemmelavet tomatsovs</li>
+                            <li>Mozzarella</li>
+                            <li>Charcuterie</li>
+
+                        </ul>
+
+                    </div>
+                    <div class="contact-info">
+                        <p>Place din order her</p>
+                        <div class="contact-phone">
+                            <i></i>
+                            <p class="phone-number">+ 45 75 12 20 57</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </article>
+        <div class="product-separator-container-wine-glass">
+            <div class="product-separator">
+            </div>
+        </div>
+        <article id="oster">
+            <div class="ost-text-info">
+                <div class="round-ornament"></div>
+                <h2>Bred vifte af oste</h2>
+                <p>Vi sælger pizzakits to dage om ugen, Torsdag og Fredag.
+                    Du køber en kit fra os, som du selv kan lave. Vi har instruktionelle videoer hernede.
+                    Gerne ordr fremadrettet, sådan så vi kan forberede din kit</p>
+            </div>
+            <div class="round-cheese-line-ornament"></div>
+            <div class="image-container">
+                <img class="portrait" src="" alt="image of the cheese shop OstEsbjerg and its owner Katherine"
+                    title="cheese shop image">
+                <img class="landscape" src="" alt="image of the cheese shop OstEsbjerg display">
+            </div>
+            <div class="about-section-one">
+                <img src="" alt="Image of Katherine cutting cheese">
+                <section class="about-text-one">
+                    <h3>Lorem ipsum </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit veniam voluptatem illum eveniet
+                        pariatur dolore, consectetur delectus illo ut. Quidem corporis explicabo officia possimus quae
+                        iure debitis voluptatem commodi eius!</p>
+                </section>
+                <div class="cheese-slice-line-ornament-container">
+                    <div class="cheese-slice-line-ornament-container"></div>
+                </div>
+            </div>
+            <div class="about-section-one">
+
+                <section class="about-text-one">
+                    <h3>Lorem ipsum </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit veniam voluptatem illum eveniet
+                        pariatur dolore, consectetur delectus illo ut. Quidem corporis explicabo officia possimus quae
+                        iure debitis voluptatem commodi eius!</p>
+                </section>
+
+                <div class="multiple-line-ornament-container">
+                    <div class="multiple-line-ornament-container"></div>
+                </div>
+                <img src="" alt="Image of Katherine in the shop">
+            </div>
+
+        </article>
+
+    </section>
+    <h2>Vi inviterer dig hjerteligt!</h2>
 </main>
 <?php get_footer()?>
