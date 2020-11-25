1fa1ad670d26d0edbbbff9ac958a31947cbade22
Raul-Octavina Albu <alburaul@gmil.com>
2020-11-25 12:11:21 +0100

    product page done for desktop

--- page-about.php
+++ page-about.php
@@ -1,29 +1,5 @@
 <?php get_header()?>
 <main class="about">
-    <?php if (have_posts()): ?>
 
-    <?php while (have_posts()): the_post()?>
-    <div class="products">
-        <nav class="site-nav">
-
-            <?php $args = array(
-        "child_of" => get_top_ancestor_id(),
-        "title_li" => "",
-    );
-    ?>
-            <ul>
-                <?php wp_list_pages($args)?>
-            </ul>
-        </nav>
-
-        <h3> <?php the_title()?> </h3>
-
-        <?php the_content()?>
-
-        <?php endwhile;?>
-    </div>
-
-    <?php endif;?>
-    <button>sudo</button>
 </main>
 <?php get_footer()?>
