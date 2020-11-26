9d2654f927e93c23ab49e6cfe9af2aa446f8489e
julia <julia.gru@gmail.com>
2020-11-26 14:02:48 +0100

    ness finished

--- index.php
+++ index.php
@@ -4,6 +4,34 @@ get_header()
 
 
 <main class="news">
+
+    <div class="news__hero">
+        <div class="news__hero__picture" style="background-image: url(<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/news-titile-pic.jpg);"></div>
+
+        <div class="news__hero__ornament1" style="background-image: url(<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/svg/cheese-roll.svg);"></div>
+
+        <div class="news__hero__ornament2" style="background-image: url(<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/svg/olives.svg);"></div>
+        <div class="news__hero__text">
+        
+            <div class="news__hero__text__wrapper">
+                <h1>Nyheder</h1>
+                <ul class="hero__recent_posts">
+                    <?php
+                        $recent_posts = wp_get_recent_posts();
+                        foreach( $recent_posts as $recent ) {
+                            printf( '<li><a href="%1$s">%2$s</a></li>',
+                                esc_url( get_permalink( $recent['ID'] ) ),
+                                apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
+                            );
+                        }
+                    ?>
+                </ul>
+                <div class="news__hero__ornament3" style="background-image: url(<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/svg/multi-element.svg);"></div>
+            </div>
+
+        </div>
+    </div>
+
     <?php
     if (have_posts()):
         while (have_posts()): the_post();?>
