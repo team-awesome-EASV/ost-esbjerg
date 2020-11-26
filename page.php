8c7e87102c55b706509cfb60d42673f1bc9c53a5
darthradek <radoslawhaller@gmail.com>
2020-11-27 00:54:12 +0100

    home page basic layout

--- page.php
+++ page.php
@@ -2,6 +2,80 @@
 get_header()
 ?>
 
+    <div class="container">
+
+        <div class="home-page-section">
+            <div class="left-wrapper">
+                <!-- picture -->
+            </div>
+
+            <div class="right-wrapper">
+                <div class="olives-ornament"><!-- olives ornament on the top --></div> 
+                <img class="home-illu" src="<?php echo get_bloginfo( 'template_directory' ); ?>/static-assets/homepage-illu.png" alt="">
+            </div>
+        </div>
+
+        <div class="info-page-section">
+            <div class="cheese-illu-wrapper"></div>
+            <div class="circle-banner-1"></div>
+            <div class="circle-banner-2"></div>
+            <div class="info-container">
+                <div class="text-container">
+                    <h1>Lorem ipsum</h1>
+                    <p>
+                       Man kan fremad se, at de har været udset til at læse,
+                       at der skal dannes par af ligheder. Dermed kan der afsluttes
+                       uden løse ender, og de kan optimeres fra oven af at formidles stort
+                       uden brug fra presse. I en kant af landet går der blandt om, at de
+                       vil sætte den over forbehold for tiden. 
+                    </p>
+                    <button>Check it out!</button>
+                </div>
+                <div class="illu-container">  <!-- fine dining illu --> </div>
+            </div>
+        </div>
+
+        <div class="video-section">
+
+        </div>
+
+        <div class="location-section">
+            <div class="content-holder">
+                <div class="row">
+                    <div class="content-wrapper image1">
+                        <!-- image -->
+                    </div>
+                    <div class="content-wrapper map">
+                        <!-- map -->
+                    </div>
+                    <div class="content-wrapper graphic1">
+                        <div class="olives-illu"></div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="content-wrapper graphic2">
+                        <div class="cheese-illu"></div>
+                    </div>
+                    <div class="content-wrapper address">
+                        <!-- map -->
+                    </div>
+                    <div class="content-wrapper image2">
+                        <!-- image --> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>  
+
+
 <?php
 get_footer()
 ?>
+
