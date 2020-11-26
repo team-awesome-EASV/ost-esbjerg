317d092f11b82b71b4bee6d697d2814ce57a5be2
Raul-Octavina Albu <alburaul@gmil.com>
2020-11-26 06:56:37 +0100

    about 50% done

--- page-about.php
+++ page-about.php
@@ -1,5 +1,38 @@
 <?php get_header()?>
 <main class="about">
+    <section class="hero">
+        <div class="olive-artwork"></div>
+        <div class="cheese-wheel-artwork"></div>
+        <div class="multi-elements-artwork"></div>
+        <div class="chevron-down"></div>
+    </section>
+
+    <div class="about-text-container">
+        <div class="text-container-one">
+            <h2>
+                Part
+            </h2>
+            <p>
+                Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan der
+                afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse. I en
+                kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
+            </p>
+
+        </div>
+        <div class="text-container-two">
+            <h2>
+                Our cheese
+            </h2>
+            <p>
+                Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan der
+                afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse. I en
+                kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
+            </p>
+        </div>
+        <div>
+
+        </div>
+    </div>
 
 </main>
 <?php get_footer()?>
