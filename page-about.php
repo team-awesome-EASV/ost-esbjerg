2f044500b6af6c0394e9bec604183a7105c24695
Raul-Octavina Albu <alburaul@gmil.com>
2020-11-26 08:34:55 +0100

    about for desktop is done

--- page-about.php
+++ page-about.php
@@ -7,32 +7,49 @@
         <div class="chevron-down"></div>
     </section>
 
-    <div class="about-text-container">
-        <div class="text-container-one">
+    <section class="about-text-container ">
+        <div class="mid-line-artwork">
+
+        </div>
+        <div class="text-container-one text-container">
             <h2>
                 Part
             </h2>
             <p>
-                Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan der
-                afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse. I en
+                Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan
+                der
+                afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse.
+                I en
                 kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
             </p>
 
         </div>
-        <div class="text-container-two">
+        <div class="text-container-two text-container">
             <h2>
                 Our cheese
             </h2>
             <p>
-                Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan der
-                afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse. I en
-                kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
+                Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. <Dermed kan
+                    der afsluttes uden løse ender, og de kan>optimeres fra oven af at formidles stort uden brug fra
+                    presse.
+                    I en
+                    kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
             </p>
         </div>
-        <div>
 
+    </section>
+    <section class="about-owner">
+        <div class="text-wrapper">
+            <div class="owner-text-container">
+                <h2>Kathrine</h2>
+                <p>Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan
+                    der afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra
+                    presse. I en kant af landet går der blandt om, at de vil sætte den over forbehold for tiden. </p>
+            </div>
         </div>
-    </div>
+
+
+    </section>
 
 </main>
 <?php get_footer()?>
