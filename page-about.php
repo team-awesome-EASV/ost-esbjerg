b78d4890e2d20ad07e5df2da1562b866154d69bd
Raul-Octavina Albu <alburaul@gmil.com>
2020-12-02 14:12:40 +0100

    acf implemented

--- page-about.php
+++ page-about.php
@@ -12,39 +12,60 @@
 
         </div>
         <div class="text-container-one text-container">
-            <h2>
+            <?php if( get_field('about_text_one_title') ): ?>
+            <h2><?php the_field('about_text_one_title'); ?></h2>
+            <?php endif; ?>
+            <!-- <h2>
                 Part
-            </h2>
-            <p>
+            </h2> -->
+            <?php if( get_field('about_text_one_content') ): ?>
+            <p><?php the_field('about_text_one_content'); ?></p>
+            <?php endif; ?>
+            <!-- <p>
                 Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan
                 der
                 afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra presse.
                 I en
                 kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
-            </p>
+            </p> -->
 
         </div>
         <div class="text-container-two text-container">
-            <h2>
+            <?php if( get_field('about_text_two_title') ): ?>
+            <h2><?php the_field('about_text_two_title'); ?></h2>
+            <?php endif; ?>
+            <!-- <h2>
                 Our cheese
-            </h2>
-            <p>
+            </h2> -->
+
+            <?php if( get_field('about_text_two_content') ): ?>
+            <p><?php the_field('about_text_two_content'); ?></p>
+            <?php endif; ?>
+            <!-- <p>
                 Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. <Dermed kan
                     der afsluttes uden løse ender, og de kan>optimeres fra oven af at formidles stort uden brug fra
                     presse.
                     I en
                     kant af landet går der blandt om, at de vil sætte den over forbehold for tiden.
-            </p>
+            </p> -->
         </div>
 
     </section>
     <section class="about-owner">
         <div class="text-wrapper">
             <div class="owner-text-container">
-                <h2>Kathrine</h2>
-                <p>Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan
+
+                <?php if( get_field('about_katherine_title') ): ?>
+                <h2><?php the_field('about_katherine_title'); ?></h2>
+                <?php endif; ?>
+                <!-- <h2>Kathrine</h2> -->
+
+                <?php if( get_field('about_katherine_content') ): ?>
+                <p><?php the_field('about_katherine_content'); ?></p>
+                <?php endif; ?>
+                <!-- <p>Man kan fremad se, at de har været udset til at læse, at der skal dannes par af ligheder. Dermed kan
                     der afsluttes uden løse ender, og de kan optimeres fra oven af at formidles stort uden brug fra
-                    presse. I en kant af landet går der blandt om, at de vil sætte den over forbehold for tiden. </p>
+                    presse. I en kant af landet går der blandt om, at de vil sætte den over forbehold for tiden. </p> -->
             </div>
         </div>
 
