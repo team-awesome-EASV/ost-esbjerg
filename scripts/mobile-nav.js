1012b92eaa0c84d4be2713ad2a1b79a3f281e1b8
Raul-Octavina Albu <alburaul@gmil.com>
2020-12-03 12:05:14 +0100

    screenshot added

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,13 +1,15 @@
 jQuery(document).ready(function() {
     jQuery('.toggle-nav-icon').click(function(e) {
         jQuery('.mobile-menu').slideToggle(400);
+
   
         e.preventDefault();
     });  
 
 
     jQuery('.toggle-nav-icon').click(function(e) {
-        jQuery('#menu-mobile-menu' ).slideToggle(400);
+        jQuery('#menu-mobile-menu').slideToggle(400);
+
   
         e.preventDefault();
     }); 
